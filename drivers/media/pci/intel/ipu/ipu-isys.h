91be238629243ef11511f03f3b78e1ff77d880e7
Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>
2025-03-17 12:10:09 +0100

    ipu6: move isys-subdev to common
    Signed-off-by: Stanislaw Gruszka <stanislaw.gruszka@linux.intel.com>

--- drivers/media/pci/intel/ipu/ipu-isys.h
+++ drivers/media/pci/intel/ipu/ipu-isys.h
@@ -2,8 +2,11 @@
 #define INTEL_IPU_ISYS_H
 
 #include <linux/list.h>
+#include <linux/container_of.h>
 
 #include <media/videobuf2-v4l2.h>
+#include <media/v4l2-ctrls.h>
+#include <media/v4l2-subdev.h>
 
 struct ipu_isys_queue {
 	struct vb2_queue vbq;
@@ -50,5 +53,44 @@ struct ipu_isys_buffer_list {
 #define ipu_isys_buffer_to_vb2_buffer(__ib) \
 	(&ipu_isys_to_isys_video_buffer(__ib)->vb_v4l2.vb2_buf)
 
+struct ipu_isys_subdev {
+	struct v4l2_subdev sd;
+	u32 const *supported_codes;
+	struct media_pad *pad;
+	struct v4l2_ctrl_handler ctrl_handler;
+	void (*ctrl_init)(struct v4l2_subdev *sd);
+	int source;	/* SSI stream source; -1 if unset */
+};
 
+#define to_ipu6_isys_subdev(__sd) \
+	container_of(__sd, struct ipu_isys_subdev, sd)
+
+unsigned int ipu_isys_mbus_code_to_bpp(u32 code);
+unsigned int ipu_isys_mbus_code_to_mipi(u32 code);
+bool ipu_isys_is_bayer_format(u32 code);
+u32 ipu_isys_convert_bayer_order(u32 code, int x, int y);
+
+int ipu_isys_subdev_set_fmt(struct v4l2_subdev *sd,
+			     struct v4l2_subdev_state *state,
+			     struct v4l2_subdev_format *fmt);
+int ipu_isys_subdev_enum_mbus_code(struct v4l2_subdev *sd,
+				    struct v4l2_subdev_state *state,
+				    struct v4l2_subdev_mbus_code_enum
+				    *code);
+u32 ipu_isys_get_src_stream_by_src_pad(struct v4l2_subdev *sd, u32 pad);
+int ipu_isys_get_stream_pad_fmt(struct v4l2_subdev *sd, u32 pad, u32 stream,
+				 struct v4l2_mbus_framefmt *format);
+int ipu_isys_get_stream_pad_crop(struct v4l2_subdev *sd, u32 pad, u32 stream,
+				  struct v4l2_rect *crop);
+int ipu_isys_subdev_set_routing(struct v4l2_subdev *sd,
+				 struct v4l2_subdev_state *state,
+				 enum v4l2_subdev_format_whence which,
+				 struct v4l2_subdev_krouting *routing);
+int ipu_isys_subdev_init(struct device *dev,
+			  struct ipu_isys_subdev *asd,
+			  const struct v4l2_subdev_ops *ops,
+			  unsigned int nr_ctrls,
+			  unsigned int num_sink_pads,
+			  unsigned int num_source_pads);
+void ipu_isys_subdev_cleanup(struct ipu_isys_subdev *asd);
 #endif
